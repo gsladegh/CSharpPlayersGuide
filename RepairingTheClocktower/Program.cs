edc87400c2289ae44bcd201e7a8a8eaba906dcb3
Grant Slade <gslade@yescommunities.com>
2023-06-24 15:47:25 -0600

    Watchtower Start

---
+++ RepairingTheClocktower/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Challenge Repairing the Clocktower 100 XP
+
+    The recent attacks damaged the great Clocktower of Consolas. The citizens of Consolas have repaired 
+    most of it, except one piece that requires the steady hand of a Programmer. It is the part that makes the 
+    clock tick and tock. Numbers flow into the clock to make it go, and if the number is even, the clock’s 
+    pendulum should tick to the left; if the number is odd, the pendulum should tock to the right. Only a 
+    programmer can recreate this critical clock element to make it work again.
+
+    Objectives:
+    • Take a number as input from the console.
+    • Display the word “Tick” if the number is even. Display the word “Tock” if the number is odd.
+    • Hint: Remember that you can use the remainder operator to determine if a number is even or odd.
+ */
+Console.Clear();
+Console.Write("Enter a clock number: ");
+int clockNumber = Convert.ToInt32(Console.ReadLine());
+
+if(clockNumber % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
---
+++ Watchtower/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Challenge Watchtower 100 XP
+ * 
+    There are watchtowers in the region around Consolas that can alert 
+    you when an enemy is spotted. With some help from you, they can tell 
+    you which direction the enemy is from the watchtower.
+
+    Objectives:
+    • Ask the user for an x value and a y value. These are coordinates of 
+    the enemy relative to the watchtower’s location.
+    • Using the image on the right, if statements, and relational 
+    operators, display a message about what direction the enemy is 
+    coming from. For example, “The enemy is to the northwest!” or 
+    “The enemy is here!”
+
+ */
+
+int x;
+int y;
+
+Console.Write("Enter x value: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter y value: ");
+y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("");
+}
