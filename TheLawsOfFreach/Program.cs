984e4ba190af328a72f50ceeeb02985a7bc21360
Grant Slade <gslade@yescommunities.com>
2023-06-26 14:14:25 -0600

    The Laws of Freach converted to foreach

--- TheLawsOfFreach/Program.cs
+++ TheLawsOfFreach/Program.cs
@@ -17,17 +17,17 @@
 // find the smallest value
 int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
 int currentSmallest = int.MaxValue; // Start higher than anything in the array. 
-for (int index = 0; index < array.Length; index++)
+foreach (int value in array)
 {
-    if (array[index] < currentSmallest)
-        currentSmallest = array[index];
+    if (value < currentSmallest)
+        currentSmallest = value;
 }
 Console.WriteLine(currentSmallest);
 
 // find the average value
-int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
 int total = 0;
-for (int index = 0; index < array.Length; index++)
-    total += array[index];
-float average = (float)total / array.Length;
+foreach (int value in array2)
+    total += value;
+float average = (float)total / array2.Length;
 Console.WriteLine(average);
