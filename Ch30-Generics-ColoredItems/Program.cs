ef5ceee1e38a160adb5977626f8db2154683e739
Grant Slade <gslade@yescommunities.com>
2023-07-10 11:46:05 -0600

    Initial commit of Chapter 30 Generics exercise

---
+++ Ch30-Generics-ColoredItems/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  Challenge Colored Items 100 XP
+ *  
+    You have a sword, a bow, and an axe in front of you, defined like this:
+
+    public class Sword { }
+    public class Bow { }
+    public class Axe { }
+
+    You want to associate a color with these items (or any item type). You could make ColoredSword
+    derived from Sword that adds a Color property, but doing this for all three item types will be 
+    painstaking. Instead, you define a new generic ColoredItem class that does this for any item.
+
+    Objectives:
+    • Put the three class definitions above into a new project.
+
+    • Define a generic class to represent a colored item. It must have properties for the item itself (generic 
+    in type) and a ConsoleColor associated with it. 
+
+    • Add a void Display() method to your colored item type that changes the console’s foreground 
+    color to the item’s color and displays the item in that color. (Hint: It is sufficient to just call 
+    ToString() on the item to get a text representation.)
+
+    • In your main method, create a new colored item containing a blue sword, a red bow, and a green axe. 
+    Display all three items to see each item displayed in its color.
+
+ */
